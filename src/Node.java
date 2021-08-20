db47cbd4dfcacf6eca5a38d5f66a000297b73069
ViniY <vincentyu9603@gmail.com>
2021-08-20 23:35:39 +1200

    General Tree Node
    Node

---
+++ src/Node.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Node {
+    Node parent;
+    ArrayList<Node> children = new ArrayList<>();
+    int numChild = 0;
+    int depth;
+    int value;
+    int nodeId = 0;
+
+    public Node(Node parent, int value){
+        this.parent = parent;
+        this.value = value;
+    }
+    public Node(Node parent, int id, int value, int depth){
+        this.parent = parent;
+        this.nodeId = id;
+        this.value = value;
+        this.depth = depth;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public ArrayList<Node> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Node> children) {
+        this.children = children;
+    }
+
+    public int getNumChild() {
+        this.numChild = this.children.size();
+        return numChild;
+    }
+
+    public void setNumChild(int numChild) {
+        this.numChild = numChild;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(int nodeId) {
+        this.nodeId = nodeId;
+    }
+}
